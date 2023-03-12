07e5ce8a0fe36593bf90f1726c935ad968da1a88
Denis Lion <levdenis01@gmail.com>
2023-03-12 23:42:13 +0700

    First Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int a, b, c;
+	cout << "a=" << endl;
+	cin >> a;
+	cout << "b=" << endl;
+	cin >> b;
+	cout << "c=" << endl;
+	cin >> c;
+
+	if (a > 0)
+	{
+		if (b > 0)
+
+		{
+			if (c > 0)
+
+			{
+				cout << "true" << endl;
+			}
+		}
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+}
